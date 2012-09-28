9220ec6ebc258e3393edcbf94311ff0769df4c6e
Lee Boynton <lee@lboynton.com>
2012-09-28 12:55:11 +0100

    Install node-gyp globally to try to workaround error

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,18 @@ class bosh {
     package {$deps:
         ensure => present,
     }
+    # workaround for error by installing dependency globally
+    package { 'node-gyp':
+        ensure   => present,
+        provider => 'npm',
+    }
     package { 'node-xmpp-bosh':
         ensure   => present,
         provider => 'npm',
         require  => [
             Class['nodejs'],
             Package[$deps],
+            Package['node-gyp'],
         ]
     }
 }
