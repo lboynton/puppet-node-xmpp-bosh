ecd91c5496a0fa65a13e6226f0e4b0384ecac819
Lee Boynton <lee@lboynton.com>
2012-09-28 12:16:05 +0100

    And install expat-devel too

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class bosh {
     include nodejs
+    package {'expat-devel':
+        ensure => present,
+    }
     package { 'node-xmpp-bosh':
         ensure   => present,
         provider => 'npm',
