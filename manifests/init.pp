f28ad24eea9f0fa33826f7b9f79d054c1fcc83e8
Lee Boynton <lee@lboynton.com>
2012-10-01 10:47:37 +0100

    Try to workaround EACESS error when creating /root/.node-gyp folder by allowing installations to be run by root.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,21 +4,19 @@ class bosh {
     package {$deps:
         ensure => present,
     }
-    # workaround for error by installing dependency globally
-    package { 'node-gyp':
-        ensure   => present,
-        provider => 'npm',
-        require  => [
-            Class['nodejs'],
-        ]
-    }
+    # Tell npm to install packages as root user instead of dropping to the
+    # nobody user due to error with node-gyp. This will hopefully be fixed
+    # when a newer version of node-gyp is used.
+    exec { 'sudo /usr/bin/npm config set unsafe-perm true':
+		alias => 'npm-unsafe-perm',
+	}
     package { 'node-xmpp-bosh':
         ensure   => present,
         provider => 'npm',
         require  => [
             Class['nodejs'],
             Package[$deps],
-            Package['node-gyp'],
+            Exec['npm-unsafe-perm'],
         ]
     }
 }
